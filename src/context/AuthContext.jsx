0bb93bf592e5c8ef4b08dc54beea812778f6eece
Malek <malekjaber39@gmail.com>
2026-04-03 22:21:59 +0300

    commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     
     const [token, setToken] = useState(localStorage.getItem('token'));
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null); 
 
     useEffect(() => {
         
